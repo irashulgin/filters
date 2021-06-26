cec9c0bfdc5f750a360a05c5504f74e7fa18f7c1
irashulgin <31027359+irashulgin@users.noreply.github.com>
2021-06-26 14:31:40 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,46 +1,8 @@
 import "./App.css";
-// import Line from "./Line";
-// import styled from "styled-components";
-// import { useState } from "react";
-// import { fieldType, getAbsentValues } from "./consts";
- import Filter from "./Filter/Filter";
-// import {
-//   Style,
-//   StyledDivSearch,
-//   Button,
-//   ButtonSearch,
-//   StyledDiv,
-// } from "./styles11";
+import Filter from "./Filter/Filter";
 
 function App() {
-  return (
-    <Filter></Filter>
-    // <Style>
-    //   {queryList.map((item, index) => (
-    //     <Line
-    //       field={item.field}
-    //       onDelete={deleteItem}
-    //       key={index}
-    //       operator={item.operator}
-    //       onChange={changeItem}
-    //       value={item.value}
-    //       index={index}
-    //       fieldList={fieldList}
-    //     />
-    //   ))}
-    //   {/* <Line operator={""}  field={""} 
-    //     value={""}  index={queryList.length}/> */}
-
-    //   <StyledDiv>
-    //     {fieldList.length > 0 && (
-    //       <Button onClick={addItem}>+ Add Criteria</Button>
-    //     )}
-    //   </StyledDiv>
-    //   <StyledDivSearch>
-    //     <ButtonSearch onClick={search}>Search</ButtonSearch>
-    //   </StyledDivSearch>
-    // </Style>
-  );
+  return <Filter></Filter>;
 }
 
 export default App;
